259e88ef363e47ffd4755e193fad52d46bd80d84
gooleh <landrover1221@gmail.com>
2024-11-05 17:02:48 +0900

    first commit

---
+++ front/ios/Pods/Target Support Files/Pods-WLNproject-WLNprojectTests/Pods-WLNproject-WLNprojectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WLNproject_WLNprojectTests : NSObject
+@end
+@implementation PodsDummy_Pods_WLNproject_WLNprojectTests
+@end
---
+++ front/ios/Pods/Target Support Files/Pods-WLNproject/Pods-WLNproject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WLNproject : NSObject
+@end
+@implementation PodsDummy_Pods_WLNproject
+@end
