ec4d9127e44f9b00bee3e98599e8ac4b8b7b1baa
overwell24 <overwell24@gmail.com>
2024-11-12 16:42:17 +0900

    feat: 라우팅 기초 완료

--- server/app.py
+++ server/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def main():
-    return "main"
+@app.route('/api/cafes', methods=['GET'])
+def get_cafes():
+    pass
+
+@app.route('/api/cafes/<int:cafe_id>', methods=['GET'])
+def get_cafe_by_id(cafe_id):
+    pass
+
+@app.route('/api/cafe/table-status', methods=['POST'])
+def update_table_status():
+    pass
