0e6762b46af189b1cb2092c7006e4b8db0c08e17
MCK-OOTS <alsmin110@google.com>
2024-11-15 18:13:30 +0900

    WIFI모듈 통신 확인

--- embedded/arduino/arduino.ino
+++ embedded/arduino/arduino.ino
@@ -1,13 +1,29 @@
-const int PressSensor = A0;
-int value = 0;
+#include <SoftwareSerial.h>
+#define rxPin 3
+#define txPin 2
+
+int PressSensor = A0;
+
+SoftwareSerial esp01(txPin, rxPin);
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //아두이노-PC통신
+  esp01.begin(9600);  //아두이노-WIFI통신
 }
 
 void loop() {
+  
   int value = analogRead(PressSensor);
   Serial.println(value);
   delay(1000);
+    
 
+  if (esp01.available()) {
+    Serial.write(esp01.read());
+  }
+
+  if(Serial.available()){
+    esp01.write(Serial.read());
+  }
 }
